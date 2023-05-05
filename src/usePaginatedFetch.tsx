cccb8381f1d062e4a0f86289f82baa82d6a7d289
ghazal <ghazal.abaszade1@gmail.com>
2023-05-05 14:22:11 +0330

    push componenmts

---
+++ src/usePaginatedFetch.tsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+import _ from 'lodash';
+
+const usePaginatedFetch = (url, pageSize) => {
+    const [loading, setLoading] = useState(true);
+    const [data, setData] = useState([]);
+
+    const getData = async () => {
+        const response = await fetch(url);
+        const data = await response.json();
+
+        const paginatedData = _.chunk(data, pageSize);
+
+        setData(paginatedData);
+        setLoading(false);
+    }
+
+    useEffect(() => {
+        getData();
+    }, []);
+
+    return [loading, data];
+}
+
+export default usePaginatedFetch;
